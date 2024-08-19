d2579153b11131bebbf60e93af3aa89c69f52f6e
Trailblazer-dev <richvictor830@gmail.com>
2024-08-19 19:55:09 +0300

    setting up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-const { description } = require('commander');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      color:{
-        bodyColor:"linear-gradient(180deg, #FAFAFA 0%, #FCFCFC 100%)",
+      colors:{        
         primaryColor:"#000000",
         secondaryColor:"#39DB4A",
         subtitleColor:"#F63B3B",
         descriptionColor:"#4A4A4A"
+      },
+      backgroundImage:{
+        bodyColor:"linear-gradient(180deg, #FAFAFA 0%, #FCFCFC 100%)",
       }
+
     },
   },
   plugins: [],
