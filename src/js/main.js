e6b548ee021f961b7a0cac067850011a16550d78
Trailblazer-dev <richvictor830@gmail.com>
2024-08-22 00:16:11 +0300

    building

--- src/js/main.js
+++ src/js/main.js
@@ -12,15 +12,20 @@ dropdownIcon.addEventListener('click', () => {
             dropdownMenu.classList.toggle('hidden');
         })
     })
-    Zone.addEventListener('mouseleave',()=>{
-        dropdownMenu.classList.toggle('hidden');
+    Zone.addEventListener('mouseleave',()=>{        
+        setTimeout(() => {
+            // code to be executed after a delay
+            dropdownMenu.classList.toggle('hidden');
+        }, 2000);
+
+        });
     })
-});
+
 // services dropdown
 const dropdown2Icon = document.getElementById("dropdown2-icon");
 const dropdown2Menu = document.getElementById("dropdown2-menu");
 const dropdown2List = document.querySelectorAll('.dropdown2-list');
-
+const zone2 = document.getElementById("zone2");
 dropdown2Icon.addEventListener('click', () => {
     dropdown2Menu.classList.toggle('hidden');
     dropdown2List.forEach(list => {
@@ -28,6 +33,13 @@ dropdown2Icon.addEventListener('click', () => {
             dropdown2Menu.classList.toggle('hidden');
         })
     })
+    zone2.addEventListener('mouseleave',()=>{        
+        setTimeout(() => {
+            // code to be executed after a delay
+            dropdown2Menu.classList.toggle('hidden');
+        }, 2000);
+
+        });
 });
 
 // mobile nav-bar
@@ -44,4 +56,10 @@ navLink.forEach(link => {
         navBar.classList.toggle('hidden');
         navMenu.classList.toggle("ri-close-large-line")
     })
+})
+
+
+window.addEventListener('scroll', ()=> {
+    const header = document.getElementById('header')
+    this.scrollY >= 58 ? header.classList.add('header-shadow') : header.classList.remove('header-shadow');
 })
